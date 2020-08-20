fab90ab348423234743370a63dca4aa8c8c85a40
George Onyango <onyangogeorge954@gmail.com>
2020-08-20 09:09:20 +0300

    add multiplication and division

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,30 @@ let add = function(number1, number2) {
     var result = add(number1, number2);
     alert(result);
 
-    let subtract = function(number1, number2) {
-        return number1 - number2:
-    }
+let subtract = function(number1, number2) {
+        return number1 - number2
+    };
 
     var number1 = parseInt(prompt("Enter a digit:"));
     var number2 =parseInt(prompt("Enter another digit:"));
     var result = subtract (number1, number2);
     alert(result);
+
+    let multiply = function(number1, number2) {
+        return number1 * number2
+    };
+
+    var number1 = parseInt(prompt("Enter a digit:"));
+    var number2 =parseInt(prompt("Enter another digit:"));
+    var result = multiply (number1, number2);
+    alert(result);
+
+    let divide = function(number1, number2) {
+        return number1 / number2
+    };
+
+    var number1 = parseInt(prompt("Enter a digit:"));
+    var number2 =parseInt(prompt("Enter another digit:"));
+    var result = divide (number1, number2);
+    alert(result);
+    
