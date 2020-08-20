21f4249882331ad1cc8fbcaf42265fcf17b6bd89
George Onyango <onyangogeorge954@gmail.com>
2020-08-20 08:44:14 +0300

    add javascript to js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,5 @@ let add = function(number1, number2) {
     
     var number1 = parseInt(prompt("Enter a number:"));
     var number2 = parseInt(prompt("Enter another number:"));
-    alert(add(number1, number2));
+    var result = add(number1, number2);
+    alert(result);
